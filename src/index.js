d05e57c2c86bcce575a82df38fe2884b1113f9cc
csm64444-dev <csm64444@gmail.com>
2026-05-18 15:41:33 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DiaryApp from '../diary-app-v2';
+import DiaryApp from './diary-app-v2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<DiaryApp />);
