426c4e419dc5d309cfc9c0134af47b06e848826f
csm64444-dev <csm64444@gmail.com>
2026-05-20 03:05:33 +0900

    Update diary-app-v2.jsx

--- diary-app-v2.jsx
+++ diary-app-v2.jsx
@@ -827,8 +827,8 @@ function NoteContent({ th, currentYear, currentMonth, selectedDay, note, onNoteC
             }}
           />
           {/* 캔버스: innerRef와 동일 크기로 오버레이 */}
-          <canvas ref={canvasRef} style={{position:"absolute",top:0,left:0,pointerEvents:"none",zIndex:2}}/>
-          <canvas ref={overlayRef} style={{position:"absolute",top:0,left:0,pointerEvents:"none",zIndex:3}}/>
+          <canvas ref={canvasRef} style={{position:"absolute",top:0,left:0,pointerEvents:"none",zIndex:2,display:isDrawMode?"block":"none"}}/>
+          <canvas ref={overlayRef} style={{position:"absolute",top:0,left:0,pointerEvents:"none",zIndex:3,display:isDrawMode?"block":"none"}}/>
           {isDrawMode&&(
             <div style={{position:"absolute",inset:0,zIndex:4,cursor:mode==="eraser"?"cell":"crosshair",touchAction:"none"}}
               onMouseDown={onDown}
